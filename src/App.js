62d40fbe57df24f60c1aa5258734bd12454b2aa2
Erdij <yildirimsukruerdi@gmail.com>
2022-12-08 21:43:52 +0300

    surface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import star from "./images/icon-star.svg";
+import thanks from "./images/illustration-thank-you.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className="wrapper">
+        <img src={star} alt="" className="bg-gray-700 p-2 rounded-full" />
+        <h2 className="text-gray-100 text-3xl my-6">How did we do?</h2>
+        <p className="text-gray-400 mb-10">
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <ul className="grid grid-cols-5 gap-5 mb-10">
+          <li>
+            <button>1</button>
+          </li>
+          <li>
+            <button>2</button>
+          </li>
+          <li>
+            <button>3</button>
+          </li>
+          <li>
+            <button>4</button>
+          </li>
+          <li>
+            <button>5</button>
+          </li>
+        </ul>
+        <div className="text-center">
+          <button className="btn-rating w-full text-white uppercase tracking-wide pt-3 pb-2 rounded-full">
+            Submit
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
